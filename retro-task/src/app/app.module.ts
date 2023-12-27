27524d1364b32b2d270c289ef0be9c1b8c67fecf
Vitor Albuquerque <vitor.ap@protonmail.com>
2023-12-27 17:05:16 -0300

    Create and style navbar with retro terminal theme
    Implemented a new navigation bar component and styled it with a retro terminal design
    
    - Created a new navbar component with responsive design using Bootstrap.
    - Added custom retro terminal aesthetics to the navbar, including a monospace font and green text on a black background.
    - Integrated a blinking cursor effect next to the 'Retro Task' brand name using CSS animations.
    - Corrected vertical alignment of navigation buttons for visual consistency.
    - Styled buttons with a retro look to match the overall theme and added interactive animations for hover and active states.
    - Ensured that the blinking effect is applied only to the cursor and not the entire text label.

--- retro-task/src/app/app.module.ts
+++ retro-task/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { TaskDetailComponent } from './task-detail/task-detail.component';
 import { TaskFormComponent } from './task-form/task-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
     TaskDetailComponent,
-    TaskFormComponent
+    TaskFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
